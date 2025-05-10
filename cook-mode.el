8346f3371edf0909af44dce83bb1491bc9be55c7
Alex Lunin <alexander.lunin@web.de>
2025-05-10 13:24:55 +0200

    fixed multiline block comments

--- cook-mode.el
+++ cook-mode.el
@@ -38,12 +38,12 @@
 
 (defvar cook-mode-syntax-table nil "Syntax table used while in `cook-mode'.")
 (setq cook-mode-syntax-table
-  (let ((st (make-syntax-table)))
-     (modify-syntax-entry ?\[ ". 1b" st)
-     (modify-syntax-entry ?\] ". 4b" st)
-     (modify-syntax-entry ?- ". 123b" st)
-     (modify-syntax-entry ?\n "> b" st)
-    st))
+      (let ((st (make-syntax-table)))
+        (modify-syntax-entry ?\[ "(]1nc" st)
+        (modify-syntax-entry ?\] ")[4nc" st)
+        (modify-syntax-entry ?- ". 123b" st)
+        (modify-syntax-entry ?\n "> b" st)
+        st))
 
 ;;; Regular Expressions ========================================================
 
