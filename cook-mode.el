4899e501c91100caece3ca3abb0a80b955113a90
Andrew Chi <chifamicom@outlook.com>
2026-05-08 19:02:09 -0400

    use more idiomatic elisp code

--- cook-mode.el
+++ cook-mode.el
@@ -18,7 +18,6 @@
 ;;
 ;;
 ;;; Code:
-(require 'generic-x)
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.cook" . cook-mode))
@@ -33,226 +32,202 @@
   :group 'cook
   :type 'boolean)
 
-(defvar image-filename-re "" "Regexp to match image filenames in comments.")
+(defcustom cook-image-filename-re (concat  "\\[-\s*\\(?3:'\\|\"\\|\\)\\(?1:.*\\."
+                                           (regexp-opt '("png" "PNG" "JPG" "jpeg"
+                                                         "jpg" "JPEG" "eps" "EPS"))
+                                           "\\)\\(?:\\3\\)\s*-\\]")
+  "Regexp to match image filenames in comments."
+  :group 'cook
+  :type 'regexp)
 
-(defcustom cook-image-width 200 "Width for preview images."
+(defcustom cook-image-width 200
+  "Width for preview images."
   :group 'cook
   :type 'natnum)
-
-(setq image-filename-re (concat  "\\[-\s*\\(?3:'\\|\"\\|\\)\\(?1:.*\\."
-                                  (regexp-opt '("png" "PNG" "JPG" "jpeg"
-                                                "jpg" "JPEG" "eps" "EPS"))
-                                  "\\)\\(?:\\3\\)\s*-\\]"))
-
-(defvar cook-mode-syntax-table nil "Syntax table used while in `cook-mode'.")
-(setq cook-mode-syntax-table
-      (let ((st (make-syntax-table)))
-        (modify-syntax-entry ?\[ "(]1nc" st)
-        (modify-syntax-entry ?\] ")[4nc" st)
-        (modify-syntax-entry ?- ". 123b" st)
-        (modify-syntax-entry ?\n "> b" st)
-        st))
 
 ;;; Regular Expressions ========================================================
 
 ;; Ingredient extraction
-(defconst cook-ingredient-re
-  "\\(?1:@\\)\\(?:\\(?2:[[:alpha:]\n\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?2:[[:alpha:]\n]*\\)\\)"
+(defconst cook-ingredient-re (rx (seq (group-n 1 "@")
+                                      (or (seq (group-n 2 (zero-or-more (any ?\n ?\s ?- alpha))) ?{
+                                               (group-n 3 (zero-or-more (not (any ?% ?}))))
+                                               (? ?% (group-n 4 (zero-or-more (not (any ?}))))) ?})
+                                          (group-n 2 (zero-or-more (any ?\n alpha))))))
   "Regular expression for matching an ingredient.
 
 Group 1: Matches @ marker.
 Group 2: Matches the ingredient.
 Group 3: Matches the quantity if available.
-")
+Gruop 4: Matches the unit if available.")
 
 ;;; Syntax Highlighting ========================================================
 
-(defvar cook-mode-font-lock nil "Font lock for `cook-mode'.")
+(defvar cook-mode-font-lock `(
+                              ;; source | author
+                              ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
+                              ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
+                              ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 2 'cook-source-author-keyword-face)
+                              ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 4 'cook-source-author-face)
 
-(setq cook-mode-font-lock `(
-    ;; source | author
-    ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
-    ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
-    ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 2 'cook-source-author-keyword-face)
-    ("\\(>>\\) \\(source\\|author\\)\\(:\\)\\(.*$\\)" 4 'cook-source-author-face)
+                              ;; time required
+                              ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
+                              ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
+                              ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 2 'cook-time-keyword-face)
+                              ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 4 'cook-time-face)
 
-    ;; time required
-    ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
-    ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
-    ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 2 'cook-time-keyword-face)
-    ("\\(>>\\) \\(time required\\)\\(:\\)\\(.*$\\)" 4 'cook-time-face)
+                              ;; course
+                              ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
+                              ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
+                              ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 2 'cook-course-keyword-face)
+                              ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 4 'cook-course-face)
 
-    ;; course
-    ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
-    ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
-    ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 2 'cook-course-keyword-face)
-    ("\\(>>\\) \\(course\\)\\(:\\)\\(.*$\\)" 4 'cook-course-face)
+                              ;; servings
+                              ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
+                              ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
+                              ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 2 'cook-servings-keyword-face)
+                              ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 4 'cook-servings-face)
 
-    ;; servings
-    ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 1 'font-lock-comment-face)
-    ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 3 'font-lock-comment-face)
-    ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 2 'cook-servings-keyword-face)
-    ("\\(>>\\) \\(servings\\)\\(:\\)\\(.*$\\)" 4 'cook-servings-face)
+                              ("\\(?1:~\\){\\(?2:[^}]*\\)}" 1 'cook-timer-char-face)
+                              ("\\(?1:~\\){\\(?2:[^}]*\\)}" 2 'cook-timer-face)
 
-    ("\\(?1:~\\){\\(?2:[^}]*\\)}" 1 'cook-timer-char-face)
-    ("\\(?1:~\\){\\(?2:[^}]*\\)}" 2 'cook-timer-face)
+                              ("\\(?1:#\\)\\(?:\\(?2:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?2:[:alpha:]*\\)\\)" 1 'cook-cookware-char-face)
+                              ("\\(?1:#\\)\\(?:\\(?2:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?2:[:alpha:]*\\)\\)" 2 'cook-cookware-face)
+                              ("\\(?1:#\\)\\(?:\\(?:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?:[:alpha:]*\\)\\)" 3 'cook-cookware-quantity-face)
 
-    ("\\(?1:#\\)\\(?:\\(?2:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?2:[:alpha:]*\\)\\)" 1 'cook-cookware-char-face)
-    ("\\(?1:#\\)\\(?:\\(?2:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?2:[:alpha:]*\\)\\)" 2 'cook-cookware-face)
-    ("\\(?1:#\\)\\(?:\\(?:[[:alpha:]\s-]*\\){\\(?3:[^}]*\\)}\\|\\(?:[:alpha:]*\\)\\)" 3 'cook-cookware-quantity-face)
-
-    (,cook-ingredient-re 1 'cook-ingredient-char-face)
-    (,cook-ingredient-re 2 'cook-ingredient-face)
-    (,cook-ingredient-re 3 'cook-ingredient-quantity-face)
-    ))
+                              (,cook-ingredient-re 1 'cook-ingredient-char-face)
+                              (,cook-ingredient-re 2 'cook-ingredient-face)
+                              (,cook-ingredient-re 3 'cook-ingredient-quantity-face))
+  "Font lock for `cook-mode'.")
 
 (defface cook-source-author-keyword-face
   '((t :inherit font-lock-keyword-face))
-  "Face for author keyword")
+  "Face for author keyword.")
 (defface cook-source-author-face
   '((t :inherit font-lock-string-face))
-  "Face for author")
+  "Face for author.")
 
 (defface cook-servings-keyword-face
   '((t :inherit font-lock-keyword-face))
-  "Face for servings keyword")
+  "Face for servings keyword.")
 
 (defface cook-servings-face
   '((t :inherit font-lock-string-face))
-  "Face for servings")
+  "Face for servings.")
 
 (defface cook-time-keyword-face
   '((t :inherit font-lock-keyword-face))
-  "Face for time required keyword")
+  "Face for time required keyword.")
 
 (defface cook-time-face
   '((t :inherit font-lock-string-face))
-  "Face for time required")
+  "Face for time required.")
 
 (defface cook-course-keyword-face
   '((t :inherit font-lock-keyword-face))
-  "Face for course keyword")
+  "Face for course keyword.")
 
 (defface cook-course-face
   '((t :inherit font-lock-string-face))
-  "Face for course")
+  "Face for course.")
 
 (defface cook-ingredient-char-face
   '((t :inherit font-lock-string-face))
-  "Face for ingredient char")
+  "Face for ingredient char.")
 
 (defface cook-ingredient-face
   '((t :inherit font-lock-keyword-face))
-  "Face for ingredient name")
+  "Face for ingredient name.")
 
 (defface cook-ingredient-quantity-face
   '((t :inherit font-lock-string-face))
-  "Face for ingredient quantity")
+  "Face for ingredient quantity.")
 
 (defface cook-cookware-char-face
   '((t :inherit font-lock-string-face))
-  "Face for cookware char")
+  "Face for cookware char.")
 
 (defface cook-cookware-face
   '((t :inherit font-lock-keyword-face))
-  "Face for cookware name")
+  "Face for cookware name.")
 
 (defface cook-cookware-quantity-face
   '((t :inherit font-lock-string-face))
-  "Face for cookware quantity")
+  "Face for cookware quantity.")
 
 (defface cook-timer-char-face
   '((t :inherit font-lock-string-face))
-  "Face for timer char")
+  "Face for timer char.")
 
 (defface cook-timer-face
   '((t :inherit font-lock-string-face))
-  "Face for timer")
+  "Face for timer.")
 
 
 ;;; Ingredient Parsing ===================================================================
 
 (defun cook-parse-ingredient (ingredient-str)
-  "Parse an ingredient string into (INGREDIENT QUANTITY UNITS)
-Example: \"@olive oil{2%tbsp}\" => (\"olive oil\" 2 \"tbsp\")"
-  (string-match cook-ingredient-re ingredient-str)
-  (let* ((m (match-data))
-         (ingredient (substring ingredient-str (nth 4 m) (nth 5 m)))
-         (quantity-str (substring ingredient-str (nth 6 m) (nth 7 m))))
-    (if (seq-contains-p quantity-str ?%)
-        (let* ((q-split (split-string quantity-str "%"))
-               (quantity (nth 0 q-split))
-               (units (nth 1 q-split)))
-          (list ingredient quantity units))
-      (list ingredient quantity-str nil))))
+  "Parse an string INGREDIENT-STR into a list.
 
+The returned is in the format (INGREDIENT QUANTITY UNITS).  For example,
+\"@olive oil{2%tbsp}\" gets parsed to (\"olive oil\" 2 \"tbsp\")."
+  (declare (ftype (function (string) list)))
+  (string-match cook-ingredient-re ingredient-str)
+  (list (match-string 2 ingredient-str)
+        (match-string 3 ingredient-str)
+        (match-string 4 ingredient-str)))
 (defun cook-ingredients-list ()
-  "Return the ingredients list for the current buffer. Each element is
-of the form (INGREDIENT QUANTITY UNITS), where UNITS can be nil."
-  (interactive)
+  "Return the ingredients list for the current buffer.
+
+Each element is of the form (INGREDIENT QUANTITY UNITS), where UNITS can be nil."
+  (declare (ftype (function () list)))
   (let ((ingredients '()))
     (save-excursion
-      (goto-char (point-min))
-      (while (and (< (point) (point-max))
-                  (re-search-forward cook-ingredient-re (point-max) t))
-        (push (match-string-no-properties 0) ingredients)))
-    (setq ingredients (nreverse ingredients))
-
-    (mapcar #'cook-parse-ingredient ingredients)))
+      (goto-char (point-max))
+      (while (re-search-backward cook-ingredient-re (point-max) t)
+        (push (cook-parse-ingredient (match-string-no-properties 0)) ingredients)))
+    ingredients))
 
 (defun cook-show-ingredients ()
-  "Show ingredients list"
+  "Show ingredients list."
   (interactive)
-  (let* ((ingredients (cook-ingredients-list))
-         (ingredients-table
-          (apply #'concat
-                 (mapcar (lambda (i)
-                           (if (nth 2 i)
-                               (format "%s\t%s\t%s\n" (nth 0 i) (nth 1 i) (nth 2 i))
-                             (format "%s\t%s\n" (nth 0 i) (nth 1 i))))
-                           ingredients))))
-    (message ingredients-table)))
-
-;;; Keymap ====================================================================
-
-(defvar cook-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "C-c i") 'cook-show-ingredients)
-    map)
-  "Keymap for Cook major mode.")
-
+  (let ((ingredients (cook-ingredients-list)))
+    (with-temp-buffer
+      (dolist (ingredient ingredients)
+        (dolist (part ingredient)
+          (insert part ?\t))
+        (delete-char 1)
+        (newline))
+      (message "%s" (buffer-string)))))
 
 ;;;###autoload
 (define-derived-mode cook-mode text-mode "cook"
-  "A mode for cooklang recipes"
-  (set-syntax-table cook-mode-syntax-table)
+  "A mode for cooklang recipes."
   (setq font-lock-defaults '(cook-mode-font-lock))
-  (make-local-variable 'comment-start)
-  (make-local-variable 'comment-end)
-  (setq comment-start "[-"
-        comment-end "-]")
-  (font-lock-add-keywords nil
-                          '((image-overlay-font-lock (0  'font-lock-keyword-face t)))
-                          t)
-    )
+  (setq-local comment-start "[-"
+              comment-end "-]")
+  (when cook-mode-show-images
+    (font-lock-add-keywords nil
+                            '((cook-mode-image-overlay-font-lock (0  'font-lock-keyword-face t))))))
 
-(defun cook-mode-toggle-show-images () "." (interactive)
-       (if (not cook-mode-show-images)
-           (progn
-             (font-lock-add-keywords nil
-                                     '((image-overlay-font-lock (0  'font-lock-keyword-face t)))
-                                     t)
-             )
-         (progn
-           (dolist (o (overlays-in (point-min) (point-max)))
-             (delete-overlay o)
-             )
-           (font-lock-remove-keywords nil
-                                      '((image-overlay-font-lock (0  'font-lock-keyword-face t)))))
-         )
-       (font-lock-flush)
-       (setq cook-mode-show-images (not cook-mode-show-images))
-       )
+(modify-syntax-entry ?\[ "(]1nc" cook-mode-syntax-table)
+(modify-syntax-entry ?\] ")[4nc" cook-mode-syntax-table)
+(modify-syntax-entry ?- ". 123b" cook-mode-syntax-table)
+(modify-syntax-entry ?\n "> b" cook-mode-syntax-table)
+
+(keymap-set cook-mode-map "C-c C-i" #'cook-show-ingredients)
+
+(defun cook-mode-toggle-show-images ()
+  "Toggle whether to show images from comments."
+  (interactive)
+  (setq cook-mode-show-images (not cook-mode-show-images))
+  (if cook-mode-show-images
+      (font-lock-add-keywords nil
+                            '((cook-mode-image-overlay-font-lock (0 'font-lock-keyword-face t))))
+    (progn
+        (mapc #'delete-overlay (overlays-in (point-min) (point-max)))
+        (font-lock-remove-keywords nil
+                                   '((cook-mode-image-overlay-font-lock (0 'font-lock-keyword-face t))))))
+  (font-lock-flush))
 
 ;; From org-mode
 (defun cook-mode-remove-overlay (ov after _beg _end &optional _len)
@@ -261,8 +236,8 @@ of the form (INGREDIENT QUANTITY UNITS), where UNITS can be nil."
     (when (and ov after)
       (delete-overlay ov))))
 
-(defun image-overlay-font-lock (&optional limit)
-  (when (re-search-forward image-filename-re limit t)
+(defun cook-mode-image-overlay-font-lock (&optional limit)
+  (when (re-search-forward cook-image-filename-re limit t)
     (let ((beg (match-beginning 0))
           (end (match-end 0))
           (imgfile (match-string 1)))
@@ -272,8 +247,7 @@ of the form (INGREDIENT QUANTITY UNITS), where UNITS can be nil."
               (ov (make-overlay beg end)))
           (overlay-put ov 'display img)
           (overlay-put ov 'face 'default)
-          (overlay-put ov 'modification-hooks
-                       (list 'cook-mode-remove-overlay)))))))
+          (overlay-put ov 'modification-hooks '(cook-mode-remove-overlay)))))))
 
 (provide 'cook-mode)
 ;;; cook-mode.el ends here
